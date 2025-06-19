71cd752921c0ad88b6b16742067652aeb3958f4a
COGMLS <mlsmatheus@hotmail.com>
2025-06-18 21:51:13 -0300

    Added WinDateTimeSync.py

---
+++ Scripts/WinDateTimeSync.py
@@ -0,0 +1,247 @@
+import http.client
+import json
+import datetime
+import sys
+import copy
+import time
+
+DEBUG_SCRIPT = True
+
+WorldTimeApiUrl = "worldtimeapi.org"
+UtcUrlPart = "/api/timezone/Etc/UTC"
+LocalUrl = "/api/timezone/America/Sao_Paulo"
+
+url = "worldtimeapi.org/api/timezone/America/Sao_Paulo"
+
+finalUrl = WorldTimeApiUrl + UtcUrlPart
+
+class HttpResponseData:
+    """
+    Http Response Data class
+    ---------------
+
+    The HttpResponseData provide multiple information about
+    the internal method 'getDateTimeInfo' including the HttpResponse,
+    but not limit to, offering additional information about
+    internal method behavior and the internal HttpResponse data
+    and Http status code.
+    """
+    _status: int
+    _response: http.client.HTTPResponse
+    _hasResponse: bool
+    _response_data: bytes
+    _http_code: int
+
+    def __init__(self):
+        self._status = -2
+        self._http_code = 0
+        self._response_data = 0x0
+        self._hasResponse = False
+        pass
+
+    def setStatus(self, status: int) -> None:
+        self._status = copy.copy(status)
+        pass
+
+    def setResponse(self, response: http.client.HTTPResponse) -> None:
+        self._hasResponse = True
+        self._response = copy.copy(response)
+        self._http_code = response.status
+        self._response_data = response.read()   # During the response copy the response data is lost. Read it and save inside a separated variable
+        pass
+
+    def getStatus(self) -> int:
+        return self._status
+    
+    def getReason(self) -> str:
+        if self._hasResponse:
+            return self.getResponse().reason
+        else:
+            return "Unknown"
+    
+    def getResponse(self) -> int | http.client.HTTPResponse:
+        if self._hasResponse:
+            return self._response
+        else:
+            return 0
+    
+    def getHttpStatus(self) -> int:
+        return self._http_code
+        
+    def getResponseData(self) -> bytes:
+        return self._response_data
+    
+def strncpy (string: str, start: int = 0, count: int = -1) -> str:
+    """
+    String N copy
+    ===============
+
+    Based on C strncpy method, this function copies the n elements.
+    This function also has support to start from a specific index.
+
+    Parameters
+    ---------------
+    
+    string: The source string to make your copy
+
+    start: Index start position. Must be zero or less than string source length
+
+    count: Number of elements you want to copy. If the count is less or greater
+    than zero, all elements will be copied
+
+    Exceptions
+    ---------------
+    
+    An exception is raised when start is greater than zero, resulting in
+    a message 'Start index out of string limits'
+
+    An exception is raised when start is less than zero, resulting in a
+    message 'Start index can not be less than zero'
+    """
+    
+    tmp = ""
+    STR_SIZE = len(string)
+
+    if start > STR_SIZE:
+        raise Exception("Start index out of string limits")
+    if start < 0:
+        raise Exception("Start index can not be less than zero")
+
+    if count < 0:
+        count = len(string)
+        pass
+    if count > STR_SIZE:
+        count = STR_SIZE
+        pass
+
+    i = start
+    iMax = start + count - 1
+
+    while i <= iMax and i <= STR_SIZE:
+        tmp += string[i]
+        i = i + 1
+        pass
+
+    return tmp
+
+def getDateTimeInfo(srvUrl: str, localUrl: str) -> HttpResponseData:
+    """
+    Get the date and time information from server
+    =============================================
+
+    This method offer a https connection to a server
+    and return the response (if not fail the server connection)
+    and the internal method behavior, like if an exception
+    occurred.
+
+    Parameters
+    ---------------------------------------------
+
+    srvUrl: Server base URL
+
+    localUrl: relative URL to use based on server URL
+
+    Return
+    ---------------------------------------------
+
+    This method return an object of 'HttpResponseData',
+    that includes the internal method status and, if not
+    fail to acquire, the HttpResponse and it's internal
+    content and status code.
+    """
+    status = -1
+    respInfo = HttpResponseData()
+    try:
+        client = http.client.HTTPSConnection(srvUrl, timeout=30)
+        client.request("GET", localUrl)
+        resp = client.getresponse()
+        respInfo.setResponse(resp)
+        client.close()
+
+        if resp.status == 200:
+            status = 0
+            pass
+        elif resp.status > 200 and resp.status < 300:
+            status = 1
+            pass
+        elif resp.status >= 300 and resp.status < 400:
+            status = 2
+            pass
+        elif resp.status >= 400 and resp.status < 500:
+            status = 3
+            pass
+        else:
+            status = 4
+            pass
+
+    except:
+        status = 5
+
+    respInfo.setStatus(status)
+    
+    return respInfo
+
+i = 1
+iMax = 10
+successOp = False
+sleepTimer = 3
+
+while i <= iMax and successOp != True:
+    respData = getDateTimeInfo(WorldTimeApiUrl, UtcUrlPart)
+    if respData.getStatus() == 0:
+        print(f"Response at trying ({i}/{iMax})\nStatus: {respData.getStatus()} Reason: {respData.getReason()}")
+        successOp = True
+        data = respData.getResponseData()
+        if DEBUG_SCRIPT:
+            print(data)
+            pass
+        break
+        pass
+    else:
+        print(f"Retrying... ({i}/{iMax})\nStatus: {respData.getStatus()} Reason: {respData.getReason()}")
+        pass
+    i = i + 1
+    time.sleep(sleepTimer)
+    pass
+
+if successOp:
+    dtJson = json.loads(respData.getResponseData())
+
+    if DEBUG_SCRIPT:
+        print(dtJson)
+        pass
+
+    utcDt = str(dtJson["utc_datetime"])
+
+    dt = datetime.datetime.fromisoformat(utcDt)
+    now = datetime.datetime.now().astimezone()
+    tzDiff = int(str(now.timetz().tzinfo))
+    delta = datetime.timedelta(0, 0, 0, 0, 0, tzDiff, 0)
+
+    if DEBUG_SCRIPT:
+        print(dt)
+        print(now)
+        print(tzDiff)
+        pass
+
+    dtFix = str(dt + delta)
+
+    if DEBUG_SCRIPT:
+        print(str(dtFix))
+        pass
+
+    dateInfo = strncpy(dtFix, 0, 10).replace('-', ',')
+    timeInfo = strncpy(dtFix, 11, 8).replace(':', ',')
+
+    if DEBUG_SCRIPT:
+        print(dateInfo)
+        print(timeInfo)
+        pass
+
+    pwshCmd = f"$datetime = System.DateTime({dateInfo},{timeInfo}); Set-Date -Date $datetime"
+
+    if DEBUG_SCRIPT:
+        print(pwshCmd)
+        pass
+    
+    pass
